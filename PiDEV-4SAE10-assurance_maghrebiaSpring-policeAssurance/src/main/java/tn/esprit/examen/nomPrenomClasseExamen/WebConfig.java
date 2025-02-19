23173c029074b86f43bf6bda9c28115c73dfc5e5
wahbisoussiesprit <wahbi.soussi@esprit.tn>
2025-02-19 12:52:31 +0100

    Minor Updates

--- PiDEV-4SAE10-assurance_maghrebiaSpring-policeAssurance/src/main/java/tn/esprit/examen/nomPrenomClasseExamen/WebConfig.java
+++ PiDEV-4SAE10-assurance_maghrebiaSpring-policeAssurance/src/main/java/tn/esprit/examen/nomPrenomClasseExamen/WebConfig.java
@@ -10,11 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200") // Permet les requêtes depuis localhost:4200
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Méthodes HTTP autorisées
-                .allowedHeaders("*") // Autorise tous les en-têtes
-                .allowCredentials(true); // Autorise l'utilisation des cookies (si nécessaire)
+        registry.addMapping("/**") 
+                .allowedOrigins("http://localhost:4200", "http://localhost:4000") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+                .allowedHeaders("*") 
+                .allowCredentials(true);
     }
 }
-
